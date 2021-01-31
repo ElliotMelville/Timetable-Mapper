740ad7ae693c7ca597c763e1ad27d30d4e372478
allistar <allistar@greentree.co.nz>
2021-01-31 22:47:02 +1300

    Free font memory

--- Timetable Mapper/TimetableMapper.Designer.cs
+++ Timetable Mapper/TimetableMapper.Designer.cs
@@ -397,6 +397,7 @@
             this.btnMoreOptions.Size = new System.Drawing.Size(146, 47);
             this.btnMoreOptions.TabIndex = 51;
             this.btnMoreOptions.Text = "MoreOptions";
+            this.btnMoreOptions.UseCompatibleTextRendering = true;
             this.btnMoreOptions.UseVisualStyleBackColor = false;
             this.btnMoreOptions.Click += new System.EventHandler(this.btnMoreOptions_Click_1);
             // 
--- Timetable Mapper/TimetableMapper.cs
+++ Timetable Mapper/TimetableMapper.cs
@@ -151,6 +151,8 @@ namespace Timetable_Mapper
             // pass the font to the font collection
             pfc.AddMemoryFont(data, fontLength);
 
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(data);
+
             btnLineOne.Font = new Font(pfc.Families[0], 8);
             btnLineTwo.Font = new Font(pfc.Families[0], 8);
             btnLineThree.Font = new Font(pfc.Families[0], 8);
@@ -159,7 +161,7 @@ namespace Timetable_Mapper
             btnLineSix.Font = new Font(pfc.Families[0], 8);
 
             btnMap.Font = new Font(pfc.Families[0], 17);
-            //btnMoreOptions.Font = new Font(pfc.Families[0], 13);
+            btnMoreOptions.Font = new Font(pfc.Families[0], 13);
             btnRandomiseColours.Font = new Font(pfc.Families[0], 13);
             btnHelp.Font = new Font(pfc.Families[0], 8);
             lblInfo.Font = new Font(pfc.Families[0], 20);
